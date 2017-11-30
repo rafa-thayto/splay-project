cbdbf1b7c0d2c231f532981f00e6bece8ee9649c
Rafael Thayto <rafa.thayto@gmail.com>
2017-11-30 21:41:22 -0200

    Added icons for differentials section and update utils.js

---
+++ website/js/utils.js
@@ -0,0 +1,13 @@
+// Carousel
+$(document).ready(function(){
+    $('.carousel').carousel({
+        shift:100,
+        dist:50
+    });
+  });
+
+// Scroll Spy
+// $(document).ready(function(){
+//     $('.scrollspy').scrollSpy();
+//   });
+        
